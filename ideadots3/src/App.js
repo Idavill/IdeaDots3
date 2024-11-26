a9d0bb6035c2784712814c0a2d23cf150ff8c61e
Idavill <idamariavilladsen@gmail.com>
2024-11-26 12:25:16 +0100

    Proof of concept: the label works in 2d for board <3

--- ideadots3/src/App.js
+++ ideadots3/src/App.js
@@ -1,12 +1,36 @@
 import React, { Suspense, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
+import { Html } from "@react-three/drei";
+import "./App.css";
+
+function Content({ time, ...props }) {
+  return (
+    <mesh {...props}>
+      <boxGeometry />x
+      <meshStandardMaterial roughness={0.75} emissive="#404057" />
+      <Html distanceFactor={10}>
+        <div className="content">
+          hello <br />
+          world
+        </div>
+      </Html>
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
     <div style={{ height: "100vh" }}>
       <Suspense fallback={<span>loading...</span>}>
-        <Canvas dpr={[1, 2]} camera={{ position: [2, 0, 4], fov: 50 }}>
+        <Canvas
+          dpr={[1, 2]}
+          camera={{
+            position: [2, 0, 4],
+            fov: 50,
+          }}
+        >
+          <Content position={[-2, 0, 0]} />
           <directionalLight position={[10, 10, 0]} intensity={1.5} />
           <directionalLight position={[-10, 10, 5]} intensity={1} />
           <directionalLight position={[-10, 20, 0]} intensity={1.5} />
