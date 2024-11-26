f18d179ad459b4a426b8221e936659e515c74abe
Idavill <idamariavilladsen@gmail.com>
2024-11-26 11:06:19 +0100

    cleanup + orbitcontrols

--- ideadots3/src/App.js
+++ ideadots3/src/App.js
@@ -1,48 +1,37 @@
 import React, { Suspense, useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useGLTF } from "@react-three/drei";
-
-// This sandbox shows how to prgressively load an asset through nested suspense blocks
-// 1. A generic fallback
-// 2. Lesser resolution
-// 3. High resolution
+import { useGLTF, OrbitControls } from "@react-three/drei";
 
 export default function App() {
   return (
-    <Suspense fallback={<span>loading...</span>}>
-      <Canvas dpr={[1, 2]} camera={{ position: [-2, 2, 4], fov: 25 }}>
-        <directionalLight position={[10, 10, 0]} intensity={1.5} />
-        <directionalLight position={[-10, 10, 5]} intensity={1} />
-        <directionalLight position={[-10, 20, 0]} intensity={1.5} />
-        <directionalLight position={[0, -10, 0]} intensity={0.25} />
-        <Rotate position-y={-0.5} scale={0.2}>
+    <div style={{ height: "100vh" }}>
+      <Suspense fallback={<span>loading...</span>}>
+        <Canvas dpr={[1, 2]} camera={{ position: [2, 0, 4], fov: 50 }}>
+          <directionalLight position={[10, 10, 0]} intensity={1.5} />
+          <directionalLight position={[-10, 10, 5]} intensity={1} />
+          <directionalLight position={[-10, 20, 0]} intensity={1.5} />
+          <directionalLight position={[0, -10, 0]} intensity={0.25} />
           <Suspense fallback={<Model url="./Assets/untitled.gltf" />}>
             <Model url="./Assets/untitled.gltf" />
           </Suspense>
-        </Rotate>
-      </Canvas>
-    </Suspense>
+          <OrbitControls
+            autoRotatex
+            autoRotateSpeed={0}
+            enableRotate={true}
+            enablePan={true}
+            enableZoom={true}
+          />
+        </Canvas>
+      </Suspense>
+    </div>
   );
 }
 
 function Model({ url, ...props }) {
-  // useGLTF suspends the component, it literally stops processing
   const { scene } = useGLTF(url);
-  // By the time we're here the model is gueranteed to be available
   return (
     <primitive object={scene} {...props}>
       {/* Added material */}
     </primitive>
-
-    // <mesh>
-    //   <boxGeometry args={[1, 1, 1]} />
-    //   <meshStandardMaterial color={"orange"} />
-    // </mesh>
   );
-}
-
-function Rotate(props) {
-  const ref = useRef();
-  useFrame((state) => (ref.current.rotation.y = state.clock.elapsedTime));
-  return <group ref={ref} {...props} />;
 }
