867b0d3a0b980a19214804ea5ec4c89335377495
Idavill <idamariavilladsen@gmail.com>
2024-11-27 10:42:07 +0100

    WIP:experiement workss

--- ideadots3/src/index.js
+++ ideadots3/src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import React, { Suspense } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+    <div className="header">
+      <span className="active">ART</span>
+      <span>ABOUT</span>
+      <span>VISIT</span>
+      <span>SHOP</span>
+    </div>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
