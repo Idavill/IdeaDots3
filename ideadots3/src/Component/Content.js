a48907dcc4262971e5d520083ca07703160d4158
Idavill <idamariavilladsen@gmail.com>
2025-01-15 11:09:50 +0100

    put content in its own file

---
+++ ideadots3/src/Component/Content.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useContext } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { SphereContext } from "./SphereContextProvider";
+import Sphere from "./Sphere";
+
+export default function Content({
+  zoom,
+  setZoom,
+  setFocus,
+  focusSphere,
+  newSphere,
+  scrollToIdea,
+  gizmo,
+  listActive,
+  controlsRef,
+  currentZoom,
+  enableCustomControls,
+  setEnableCustomControls,
+}) {
+  const context = useContext(SphereContext);
+
+  useEffect(() => {
+    if (newSphere) {
+      addNewSphere(newSphere);
+    }
+  }, [newSphere]);
+
+  const addNewSphere = (ns) => {
+    const newS = {
+      id: uuidv4(),
+      position: { x: ns.x, y: ns.y, z: ns.z },
+      title: "New Idea",
+      text: "New Idea Text",
+      img: "",
+    };
+    context.setSpheres((prevSpheres) => [...prevSpheres, newS]);
+    const newSTitleID = newS.id + "title";
+    const newSTextID = newS.id + "text";
+    localStorage.setItem(newSTitleID, newS.title);
+    localStorage.setItem(newSTextID, newS.text);
+  };
+
+  const sphereList = () => {
+    return context.spheres.map((s, i) => (
+      <>
+        <Sphere
+          listActive={listActive}
+          gizmo={gizmo}
+          zoomToView={(focusRef) => (
+            setZoom(!zoom), setFocus(focusRef), scrollToIdea(s, i)
+          )}
+          key={i}
+          position={[s.position.x, s.position.y, s.position.z]}
+          title={s.title}
+          text={s.text}
+          focus={focusSphere}
+          id={s.id}
+          controlsRef={controlsRef}
+          currentZoom={currentZoom}
+          enableCustomControls={enableCustomControls}
+          setEnableCustomControls={(e) => setEnableCustomControls(e)}
+        />
+      </>
+    ));
+  };
+
+  return <>{sphereList()} </>;
+}
--- ideadots3/src/Component/ThreeDContainer.js
+++ ideadots3/src/Component/ThreeDContainer.js
@@ -1,84 +1,12 @@
-import React, {
-  Suspense,
-  useEffect,
-  useRef,
-  useState,
-  useContext,
-} from "react";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 import "./ThreeDContainer.css";
 import * as THREE from "three";
 import CameraControls from "camera-controls";
-import { v4 as uuidv4 } from "uuid";
-import { SphereContext } from "./SphereContextProvider";
-import Sphere from "./Sphere";
+import Content from "./Content";
 
 CameraControls.install({ THREE });
-
-function Content({
-  zoom,
-  setZoom,
-  setFocus,
-  focusSphere,
-  newSphere,
-  scrollToIdea,
-  gizmo,
-  listActive,
-  controlsRef,
-  currentZoom,
-  enableCustomControls,
-  setEnableCustomControls,
-}) {
-  const context = useContext(SphereContext);
-
-  useEffect(() => {
-    if (newSphere) {
-      addNewSphere(newSphere);
-    }
-  }, [newSphere]);
-
-  const addNewSphere = (ns) => {
-    const newS = {
-      id: uuidv4(),
-      position: { x: ns.x, y: ns.y, z: ns.z },
-      title: "New Idea",
-      text: "New Idea Text",
-      img: "",
-    };
-    context.setSpheres((prevSpheres) => [...prevSpheres, newS]);
-    const newSTitleID = newS.id + "title";
-    const newSTextID = newS.id + "text";
-    localStorage.setItem(newSTitleID, newS.title);
-    localStorage.setItem(newSTextID, newS.text);
-  };
-
-  const sphereList = () => {
-    return context.spheres.map((s, i) => (
-      <>
-        <Sphere
-          listActive={listActive}
-          gizmo={gizmo}
-          zoomToView={(focusRef) => (
-            setZoom(!zoom), setFocus(focusRef), scrollToIdea(s, i)
-          )}
-          key={i}
-          position={[s.position.x, s.position.y, s.position.z]}
-          title={s.title}
-          text={s.text}
-          focus={focusSphere}
-          id={s.id}
-          controlsRef={controlsRef}
-          currentZoom={currentZoom}
-          enableCustomControls={enableCustomControls}
-          setEnableCustomControls={(e) => setEnableCustomControls(e)}
-        />
-      </>
-    ));
-  };
-
-  return <>{sphereList()} </>;
-}
 
 function CustomControls({
   setCurrentZoom,
@@ -91,14 +19,6 @@ function CustomControls({
 }) {
   const { camera, gl } = useThree();
 
-  // useFrame(() => {
-  //   if (controlsRef.current) {
-  //     const z = controlsRef.current.object.position.z; // Get the current zoom value
-  //     console.log("Current Zoom: ", z); // Log or use the zoom value as needed
-  //     setCurrentZoom(z);
-  //   }
-  // });
-
   useEffect(() => {
     if (controlsRef.current) {
       controlsRef.current.enableZoom = true;
@@ -107,7 +27,6 @@ function CustomControls({
 
   useFrame((state, delta) => {
     if (controlsRef.current) {
-      // Adjust the target based on zoom and focus
       const target = new THREE.Vector3();
       if (zoom) {
         target.set(focus.x, focus.y, focus.z); // Set target to focus point
