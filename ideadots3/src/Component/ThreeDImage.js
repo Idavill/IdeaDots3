4f59538451bf2082470807199b5043395095371d
Idavill <idamariavilladsen@gmail.com>
2025-01-18 07:50:39 +0100

    WWIP: images downloaded and shown in 3D but performance is very bad

--- ideadots3/src/Component/ThreeDImage.js
+++ ideadots3/src/Component/ThreeDImage.js
@@ -1,41 +1,62 @@
 import { PivotControls, Billboard, Text, Image } from "@react-three/drei";
 import React, { useEffect, useRef, useState, useContext, useMemo } from "react";
 import { db } from "./Database";
+import { useLiveQuery } from "dexie-react-hooks";
+import { v4 as uuidv4 } from "uuid";
 import testImage from "/Users/idavilladsen/Desktop/IdeaDots3/ideadots3/src/Assets/shelf.jpg";
 
 export default function ThreeDImage({ ideaId, pos }) {
   const [selectedImages, setSelectedImages] = useState([]);
+  const [url, setUrl] = useState([]);
+
+  const data = useLiveQuery(async () => {
+    return await db.images.where("ideaId").equals(ideaId).toArray();
+  });
 
   useEffect(() => {
-    const downloadImages = async () => {
-      const data = await db.images.where("ideaId").equals(ideaId).toArray();
-      if (data.length > 0) {
+    downloadImages(ideaId);
+    console.log(ideaId);
+  }, [ideaId, data]);
+
+  useEffect(() => {
+    if (url.length === selectedImages.length) {
+      console.log("yes");
+    }
+  }, [url]);
+
+  const downloadImages = async () => {
+    try {
+      //const data = await db.images.where("ideaId").equals(ideaId).toArray();
+      console.log("DATA: ", data);
+      if (data) {
         const blobs = data.map((item) => item.image);
         setSelectedImages(blobs);
       }
-    };
+      srcList();
+    } catch (e) {
+      console.log("failed to fetch images: ", e);
+    }
+  };
 
-    downloadImages(ideaId);
-    console.log(ideaId);
-  }, [ideaId]);
-
-  const imageList = () => {
-    //console.log("Selected Images: ", selectedImages);
-    return selectedImages.map((src, index) => {
-      const objectUrl = URL.createObjectURL(src);
-      return (
-        <Billboard key={index}>
-          <Image
-            transparent
-            radius={0.3}
-            position={pos}
-            scale={[3, 3, 3]}
-            url={testImage}
-          />
-        </Billboard>
-      );
+  const srcList = () => {
+    selectedImages.forEach((src) => {
+      setUrl((prev) => [...prev, URL.createObjectURL(src)]);
+      console.log(URL.createObjectURL(src));
     });
   };
 
-  return <>{imageList()}</>;
+  const imageList = url.map((src) => (
+    //const objectUrl = URL.createObjectURL(src);
+    <Billboard key={uuidv4()}>
+      <Image
+        transparent
+        radius={0.3}
+        position={pos}
+        scale={[3, 3, 3]}
+        url={src}
+      />
+    </Billboard>
+  ));
+
+  return <>halloo{imageList}</>;
 }
