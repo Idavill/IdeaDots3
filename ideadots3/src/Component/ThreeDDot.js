9aad6733ec1cfd8fefa353b1e4a891bf1eabc188
Idavill <idamariavilladsen@gmail.com>
2025-01-20 19:57:28 +0100

    ---base line: make explosion fo lines from the center of the idea position

--- ideadots3/src/Component/ThreeDDot.js
+++ ideadots3/src/Component/ThreeDDot.js
@@ -64,7 +64,7 @@ export default function ThreeDDot({
                   justifyContent: "center",
                   width: "200px",
                   height: "200px",
-                  //background: "red",
+                  background: "red",
                   opacity: "50%",
                   borderRadius: "100px",
                   position: "absolute",
