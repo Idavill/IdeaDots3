aad72f1861aba8d0be148bd365d829cbfc954c30
Idavill <idamariavilladsen@gmail.com>
2025-01-26 12:07:00 +0100

    update imports on test

--- ideadots3/tests/components/Button.test.tsx
+++ ideadots3/tests/components/Button.test.tsx
@@ -1,15 +1,23 @@
 //import { test, expect, describe, it } from 'vitest'
-import {render,screen} from '@testing-library/react'
-import Button from '../../src/Component/Button'
-import React from 'react'
-import '@testing-library/jest-dom/vitest' // with jest change to jest
+import { render, screen } from "@testing-library/react";
+import Button from "../../src/Component/Button";
+import React from "react";
+import "@testing-library/jest-dom/vitest"; // with jest change to jest
+import { describe, expect, it } from "vitest";
 
-describe('Button', () => {
-    it('should render the text being passed as a prop', () => {
-        const textParam = "click here!";
-        render(<Button onClick={()=>{console.log("clicked!")}}text={textParam}/>);
-        const button= screen.getByRole('button',{name: textParam});
-        expect(button).toBeInTheDocument();
-        expect(button).toHaveTextContent(textParam);
-    })
-})
+describe("Button", () => {
+  it("should render the text being passed as a prop", () => {
+    const textParam = "click here!";
+    render(
+      <Button
+        onClick={() => {
+          console.log("clicked!");
+        }}
+        text={textParam}
+      />
+    );
+    const button = screen.getByRole("button", { name: textParam });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(textParam);
+  });
+});
